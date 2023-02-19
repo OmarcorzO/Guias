1b0531e63c0c7ae27d16090e76ff3b32d503430d
OmarC <omarcorzo2014@outlook.com>
2023-02-19 14:57:12 -0500

    Guia 4.1 realizada con exito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'users',
+    loadChildren: () => import('./users/users.module').then( m => m.UsersPageModule)
+  },
 ];
 
 @NgModule({
